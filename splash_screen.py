66bd74775523cd45861949849ac3cec4eb4a8d61
Rustambek Shodiyev <156027859+RustambekSh@users.noreply.github.com>
2025-01-21 07:08:50 -0600

    Update splash_screen.py

--- splash_screen.py
+++ splash_screen.py
@@ -7,31 +7,23 @@ from styles import apply_font
 class SplashScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-        # Layout
         layout = BoxLayout(orientation="vertical")
 
-        # Set background color
         with layout.canvas.before:
-            Color(0.52, 0.66, 0.28, 1)  # Light green background
+            Color(0.52, 0.66, 0.28, 1)  
             self.rect = Rectangle(pos=layout.pos, size=layout.size)
-
-        # Bind the rectangle size to the layout size
         layout.bind(pos=self.update_rect, size=self.update_rect)
-
-        # App Name Label
         self.app_name_label = Label(
             text="UzHealthAI",
             font_size=48,
             bold=True,
-            color=(0.07, 0.21, 0.14, 1)  # Dark green
+            color=(0.07, 0.21, 0.14, 1) 
         )
-        apply_font(self.app_name_label)  # Apply custom font
+        apply_font(self.app_name_label)  
         layout.add_widget(self.app_name_label)
 
         self.add_widget(layout)
 
     def update_rect(self, instance, value):
-        # Update the rectangle size and position when the layout changes
         self.rect.pos = instance.pos
         self.rect.size = instance.size
