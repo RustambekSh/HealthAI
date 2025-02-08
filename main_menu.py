02ee29547faf14a71b1d0e9ee1c0110236df4d8c
Rustambek Shodiev <156027859+RustambekSh@users.noreply.github.com>
2025-02-07 20:56:39 -0600

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -6,7 +6,7 @@ from kivy.uix.label import Label
 from kivy.graphics import Color, Rectangle
 from styles import apply_font
 from languages import LANGUAGES
-from kivy.app import App 
+from kivy.app import App  
 
 class MainMenuScreen(Screen):
     def __init__(self, **kwargs):
