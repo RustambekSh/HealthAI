4ea5a4f59a76e2f394c67ee6592f9e789c214745
Rustambek Shodiev <156027859+RustambekSh@users.noreply.github.com>
2025-02-21 19:08:46 -0600

    Update splash_screen.py

--- splash_screen.py
+++ splash_screen.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import Color, Rectangle 
 from styles import apply_font
- 
+  
 class SplashScreen(Screen):  
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
