0bd6424717acd731dc27f3f23554ab8c30680992
Rustambek Shodiev <156027859+RustambekSh@users.noreply.github.com>
2025-03-10 18:27:01 -0500

    Update symptom_checker.py

--- symptom_checker.py
+++ symptom_checker.py
@@ -1,6 +1,6 @@
 from kivy.uix.screenmanager import Screen 
 from kivy.uix.button import Button 
-from kivy.uix.label import Label
+from kivy.uix.label import Label 
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
