7f83d35ee6d634d71dd84d0e99c1c30bf022435c
Rustambek Shodiev <156027859+RustambekSh@users.noreply.github.com>
2025-04-02 17:26:07 -0500

    Update clinic_finder.py

--- clinic_finder.py
+++ clinic_finder.py
@@ -6,7 +6,7 @@ from kivy.graphics import Color, Rectangle
 from kivy.properties import StringProperty
 from styles import apply_font
 import webbrowser
-from kivy.app import App
+from kivy.app import App. 
 from languages import LANGUAGES
  
 class ClinicFinderScreen(Screen):
