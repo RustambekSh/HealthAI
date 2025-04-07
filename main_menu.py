37ae3d0361155931247e0c681767648178a66e93
Rustambek Shodiev <156027859+RustambekSh@users.noreply.github.com>
2025-04-07 14:05:33 -0500

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -2,7 +2,7 @@ from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+from kivy.uix.label import Label 
 from kivy.graphics import Color, Rectangle
 from styles import apply_font
 from languages import LANGUAGES
