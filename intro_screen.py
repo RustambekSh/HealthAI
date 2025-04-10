5046f80bc8a24b9b96b3f23754ce4b7f9d94b25e
Rustambek Shodiev <156027859+RustambekSh@users.noreply.github.com>
2025-04-10 06:42:02 -0500

    Update intro_screen.py

--- intro_screen.py
+++ intro_screen.py
@@ -2,7 +2,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import Color, Rectangle 
-from kivy.clock import Clock
+from kivy.clock import Clock 
 from styles import apply_font
 from languages import LANGUAGES 
 from kivy.app import App 
