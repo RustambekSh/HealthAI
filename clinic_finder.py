03fe06f98643d8d4bb4845b43cb08c8d0eecdd3c
Rustambek Shodiev <156027859+RustambekSh@users.noreply.github.com>
2025-04-15 09:57:30 -0500

    Update clinic_finder.py

--- clinic_finder.py
+++ clinic_finder.py
@@ -1,6 +1,6 @@
 from kivy.uix.screenmanager import Screen  
 from kivy.uix.button import Button 
-from kivy.uix.label import Label 
+from kivy.uix.label import Label  
 from kivy.uix.boxlayout import BoxLayout 
 from kivy.graphics import Color, Rectangle  
 from kivy.properties import StringProperty 
